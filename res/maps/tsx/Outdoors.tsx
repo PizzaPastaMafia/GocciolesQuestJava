d4ad14be364a95f571ec1e7ce1505802860b06df
PizzaPastaMafia <lorendelfo@protonmail.com>
2022-04-10 13:54:32 +0200

    added map files

---
+++ res/maps/tsx/Outdoors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Outdoors" tilewidth="32" tileheight="32" tilecount="468" columns="12">
+ <image source="Downloads/PC Computer - Stardew Valley - Outdoors Spring.png" width="400" height="1264"/>
+</tileset>
---
+++ res/maps/tsx/farms.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="farms" tilewidth="16" tileheight="16" tilecount="1425" columns="57">
+ <image source="Downloads/PC Computer - Stardew Valley - Farm Buildings.png" width="919" height="412"/>
+</tileset>
